44f42b0bb495680790b4955b4feb136575b910ba
Robin Muller <robinmullerpython@icloud.com>
2023-10-07 19:00:49 +0200

    Installation and configuration of the posgreSQL database

--- artificial_intelligence_computer_vision_api/settings.py
+++ artificial_intelligence_computer_vision_api/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'artificial_intelligence_computer_vision_api.wsgi.application
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "robinmuller",
+        "USER": "robinmuller",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
