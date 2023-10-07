4013d7f434b534bc39fc1aacdcf0f3f370c2bb11
Robin Muller <robinmullerpython@icloud.com>
2023-10-07 19:57:10 +0200

    add Image model

--- face/models.py
+++ face/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    faces = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.image.name
