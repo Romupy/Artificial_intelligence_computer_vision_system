26a103825842d8927de509bb3d36aaac5044f348
Robin Muller <robinmullerpython@icloud.com>
2023-11-13 07:44:51 +0100

    improve detection algorithm

--- face/face.py
+++ face/face.py
@@ -1,10 +1,7 @@
 import os
-import time
-
 import cv2
 import uuid
 from statistics import median
-
 import numpy as np
 
 
@@ -23,8 +20,29 @@ class Face:
     __skin_brightness_detection(coordinates, sizes, gray_image_name):
         Detects facial skin brightness.
     __skin_type_detection(result): Determines the type of facial skin.
-    __remove_double_detection(rectangles): Removes rectangles containing others.
+    __remove_double_detection(rectangles): Removes rectangles containing
+    others.
     """
+
+    PATH = 'face/classifiers/'
+    CASCADE_PATHS = [
+        os.path.abspath(PATH + 'haarcascade_frontalface_default.xml'),
+        os.path.abspath(PATH + 'haarcascade_profileface.xml'),
+    ]
+    ALGORITHM_PARAMETERS = [
+        {'scale_factor': 1.45, 'min_neighbors': 5},
+        {'scale_factor': 1.48, 'min_neighbors': 6},
+        {'scale_factor': 1.55, 'min_neighbors': 6},
+        {'scale_factor': 1.60, 'min_neighbors': 6},
+        {'scale_factor': 1.70, 'min_neighbors': 6}
+    ]
+    MIN_SIZE_RATIO = 13
+
+    ROTATIONS = [0, 2, -2, 4, -4, 6, -6, 8, -8, 10, -10, 20, -20, 30, -30, 40,
+                 -40, 50, -50, 60, -60, 70, -70, 80, -80, 180, -180, 175, -175,
+                 170, -170, 160, -160, 150, -150, 140, -140, 130, -130, 120,
+                 -120, 110, -110, 100, -100, 0]
+
     def __init__(self, image_name):
         """
         Initializes a Face object.
@@ -49,68 +67,31 @@ class Face:
         Example: {'Number_of_faces_detected': 2, 'facial_skin': [...]}
         """
         gray = None
-        cascade_paths = [
-            os.path.abspath('face/classifiers/haarcascade_frontalface_default.xml'),
-            os.path.abspath('face/classifiers/haarcascade_frontalface_alt_tree.xml'),
-            os.path.abspath('face/classifiers/haarcascade_frontalface_alt2.xml'),
-            os.path.abspath('face/classifiers/haarcascade_profileface.xml'),
-        ]
+        faces_detected_results = None
+        img = None
 
-
-        for rotation in [0, 2, -2, 4, -4, 6, -6, 8, -8, 10, -10, 20, -20, 30, -30, 40, -40, 50, -50, 60, -60, 70, -70, 80, -80, 180, -180, 175, -175, 170, -170, 160, -160, 150, -150, 140, -140, 130, -130, 120, -120, 110, -110, 100, -100, 0]:
+        for rotation in self.ROTATIONS:
             faces_detected_results = []
             img = cv2.imread(self.image_name)
-            img = rotate_image(img, rotation)
-            for cascade_path in cascade_paths:
+            img = self.__rotate_image(img, rotation)
+            for cascade_path in self.CASCADE_PATHS:
                 face_cascade = cv2.CascadeClassifier(cascade_path)
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 black_and_white = cv2.equalizeHist(gray)
                 height, width, channels = img.shape
-                min_size_height = int(height / 13)
-                min_size_width = int(width / 13)
+                min_size_height = int(height / self.MIN_SIZE_RATIO)
+                min_size_width = int(width / self.MIN_SIZE_RATIO)
                 size = min([min_size_height, min_size_width])
-                faces_detected = face_cascade.detectMultiScale(
-                    black_and_white,
-                    scaleFactor=1.45, minNeighbors=5,
-                    minSize=(size, size),
-                    flags=cv2.CASCADE_SCALE_IMAGE
-                )
-                if len(faces_detected) > 0:
-                    faces_detected_results.append(np.array(faces_detected))
-
-                faces_detected = face_cascade.detectMultiScale(
-                    black_and_white,
-                    scaleFactor=1.48, minNeighbors=6,
-                    minSize=(size, size),
-                    flags=cv2.CASCADE_SCALE_IMAGE
-                )
-                if len(faces_detected) > 0:
-                    faces_detected_results.append(np.array(faces_detected))
-                faces_detected = face_cascade.detectMultiScale(
-                    black_and_white,
-                    scaleFactor=1.55, minNeighbors=6,
-                    minSize=(size, size),
-                    flags=cv2.CASCADE_SCALE_IMAGE
-                )
-                if len(faces_detected) > 0:
-                    faces_detected_results.append(np.array(faces_detected))
-                faces_detected = face_cascade.detectMultiScale(
-                    black_and_white,
-                    scaleFactor=1.60, minNeighbors=6,
-                    minSize=(size, size),
-                    flags=cv2.CASCADE_SCALE_IMAGE
-                )
-                if len(faces_detected) > 0:
-                    faces_detected_results.append(np.array(faces_detected))
-                faces_detected = face_cascade.detectMultiScale(
-                    black_and_white,
-                    scaleFactor=1.70, minNeighbors=6,
-                    minSize=(size, size),
-                    flags=cv2.CASCADE_SCALE_IMAGE
-                )
-                if len(faces_detected) > 0:
-                    faces_detected_results.append(np.array(faces_detected))
-
+                for algorithm_parameters in self.ALGORITHM_PARAMETERS:
+                    faces_detected = face_cascade.detectMultiScale(
+                        black_and_white,
+                        scaleFactor=algorithm_parameters['scale_factor'],
+                        minNeighbors=algorithm_parameters['min_neighbors'],
+                        minSize=(size, size),
+                        flags=cv2.CASCADE_SCALE_IMAGE
+                    )
+                    if len(faces_detected) > 0:
+                        faces_detected_results.append(np.array(faces_detected))
             if len(faces_detected_results) > 0:
                 break
         first_iteration = True
@@ -125,7 +106,6 @@ class Face:
         sorted_faces = sorted(results, key=lambda x: x[2] * x[3], reverse=True)
         faces = self.__remove_double_detection(sorted_faces)
         faces = self.__remove_containing_rectangles(faces)
-
         skin_brightness = []
         _, img_extension = os.path.splitext(self.image_name)
         gray_image_name = "images/" + str(uuid.uuid4()) + img_extension.lower()
@@ -237,7 +217,6 @@ class Face:
                             and y_i + h_i >= y_j + h_j):
                         is_contained = True
                         break
-
             if not is_contained:
                 retained_rectangles.append((x_i, y_i, w_i, h_i))
         return retained_rectangles
@@ -247,40 +226,46 @@ class Face:
         """
         Method that removes rectangles containing others from the list.
 
-        This method takes a list of rectangles represented as tuples (x, y, w, h).
-        It removes rectangles that contain other rectangles, even if there is partial overlap.
+        This method takes a list of rectangles represented as tuples (x, y, w,
+        h).
+        It removes rectangles that contain other rectangles, even if there is
+        partial overlap.
 
         Keyword arguments:
-        rectangles -- List of rectangles to be processed.
-                      Represented as tuples (x, y, w, h).
+        rectangles -- List of rectangles to be processed. Represented as
+        tuples (x, y, w, h).
 
         Returns:
         List -- List of rectangles that do not contain any other rectangles.
         """
         retained_rectangles = []
-
         for i in range(len(rectangles)):
             x_i, y_i, w_i, h_i = rectangles[i]
             is_contained_by_other = any(
-                (
-                    x_i < x_r + w_r
-                    and x_i + w_i > x_r
-                    and y_i < y_r + h_r
-                    and y_i + h_i > y_r
-                )
+                (x_i < x_r + w_r and x_i + w_i > x_r and y_i < y_r + h_r
+                 and y_i + h_i > y_r)
                 for (x_r, y_r, w_r, h_r) in retained_rectangles
             )
-
             if not is_contained_by_other:
                 retained_rectangles.append((x_i, y_i, w_i, h_i))
-
         return retained_rectangles
 
+    @staticmethod
+    def __rotate_image(img, angle):
+        """
+        Method that rotates an image.
 
-def rotate_image(img, angle):
-    height, width = img.shape[:2]
-    center = (width // 2, height // 2)
-    rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated_image = cv2.warpAffine(img, rotation_matrix, (width, height))
-    return rotated_image
+        This method rotates an image by the specified angle.
 
+        Keyword arguments:
+        img -- Image to be rotated.
+        angle -- Angle in degrees.
+
+        Returns:
+        Image -- Rotated image.
+        """
+        height, width = img.shape[:2]
+        center = (width // 2, height // 2)
+        rotation_matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated_image = cv2.warpAffine(img, rotation_matrix, (width, height))
+        return rotated_image
