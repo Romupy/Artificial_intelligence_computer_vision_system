9d3aff8a63a4b6dc1955b181c069e3f8356bae6a
Robin Muller <robinmullerpython@icloud.com>
2023-11-15 18:51:48 +0100

    refactor Image model

--- face/models.py
+++ face/models.py
@@ -4,8 +4,6 @@ from .face import Face
 
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    faces = models.IntegerField(default=0)
 
     def __str__(self):
         return {"image_name": self.image.name}
