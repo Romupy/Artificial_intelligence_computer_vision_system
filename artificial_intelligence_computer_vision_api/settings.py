de1f8ffaa10be91878de409854a0d8a1c4a44d3e
Robin Muller <robinmullerpython@icloud.com>
2023-11-15 18:35:07 +0100

    change database config

--- artificial_intelligence_computer_vision_api/settings.py
+++ artificial_intelligence_computer_vision_api/settings.py
@@ -75,22 +75,21 @@ WSGI_APPLICATION = 'artificial_intelligence_computer_vision_api.wsgi.application
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "robinmuller",
-        "USER": "robinmuller",
-        "PASSWORD": "",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "robinmuller",
+#         "USER": "robinmuller",
+#         "PASSWORD": "",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
 #     }
 # }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
